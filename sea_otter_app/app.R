0a3b0baa08f6dfcbc1c2216b8f70983c2cb545b6
kristingill <kristin_gill@ucsb.edu>
2021-01-24 16:14:31 -0800

    initial app commit - created a css file - wrangled data in an rmarkdown - the app is working, but the plot is not responding to input for some reason

---
+++ sea_otter_app/app.R
@@ -0,0 +1,43 @@
+# sea otter app
+
+library(tidyverse)
+library(shiny)
+library(here)
+library(janitor)
+
+ui <- fluidPage(theme = "ocean.css",
+
+    navbarPage("Southern Sea Otters",
+               tabPanel("Thing 1",
+                        sidebarLayout(
+                            sidebarPanel("Sea Otter Strandings",
+                                         checkboxGroupInput(inputId = "pick_life_stage",
+                                                            label = "Choose Life Stage:",
+                                                            choices = unique(sea_otter_data$life_stage))),
+                            mainPanel(plotOutput("ls_plot"))
+                        )
+                        ),
+               tabPanel("Thing 2"),
+               tabPanel("Thing 3")
+               )
+)
+
+server <- function(input, output) {
+
+  ls_reactive <- reactive({
+
+    sea_otter_data %>%
+      filter(life_stage %in% input$pick_species)
+  })
+
+  output$ls_plot <- renderPlot(
+    ggplot(data = ls_reactive(), aes(x = year,
+                                     y = count)) +
+      geom_col(aes(fill = life_stage)) +
+      theme_minimal()
+  )
+
+}
+
+shinyApp(ui = ui, server = server)
+
